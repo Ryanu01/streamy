38c4adde3a2553df4caebd55243e0cdf208708bd
Ryanu01 <ryangomes08090109@gmail.com>
2026-02-16 23:59:51 +0530

    redirect added to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+"use client"
 import { motion } from "framer-motion";
 import NavBar from "@/components/NavBar";
+import { redirect, useRouter } from "next/navigation";
 
 export default async function Home() {
-
+    const router = useRouter()
     return <div className="max-w-7xl mx-auto flex flex-col gap-4 p-4">
+        {redirect("/landing")}
         <NavBar/>
     </div>
 }
