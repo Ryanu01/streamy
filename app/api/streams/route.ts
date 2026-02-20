a3e8c11d91468fbe6e81a261d71219560307d463
Ryanu01 <ryangomes08090109@gmail.com>
2026-02-20 21:37:59 +0530

    add get route to get all spaces from user

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,13 +1,15 @@
 import prisma from "@/app/db";
+import { authOptions } from "@/app/lib/auth";
 import { CreateSpaceSchema } from "@/app/utils/types";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST (req: NextRequest) {
+export async function POST(req: NextRequest) {
     try {
         const body = await req.json()
         const { data, success } = CreateSpaceSchema.safeParse(body)
-        
-        if(!success) {
+
+        if (!success) {
             return NextResponse.json({
                 message: "Invalid schema while creating space"
             }, {
@@ -21,7 +23,7 @@ export async function POST (req: NextRequest) {
             }
         })
 
-        if(!userExist) {
+        if (!userExist) {
             return NextResponse.json({
                 message: "User does not exist"
             }, {
@@ -34,7 +36,7 @@ export async function POST (req: NextRequest) {
                 data: {
                     hostId: userExist.id,
                     name: data.name
-                }    
+                }
             })
 
             await tnx.spaceMember.create({
@@ -48,7 +50,7 @@ export async function POST (req: NextRequest) {
             return createSpace
         })
 
-        
+
 
         return NextResponse.json({
             message: "Space created successfully",
@@ -56,9 +58,47 @@ export async function POST (req: NextRequest) {
         })
     } catch (error) {
         console.log(error);
-        
+
         return NextResponse.json({
             message: "Error while creating space"
+        }, {
+            status: 500
+        })
+    }
+}
+
+export async function GET(req: NextRequest) {
+    try {
+        const session = await getServerSession(authOptions)
+
+        if (!session?.user?.id) {
+            return NextResponse.json({
+                message: "Unauthorized"
+            }, {
+                status: 401
+            });
+        }
+
+        const spaceDb = await prisma.space.findMany({
+            where: {
+                hostId: Number(session?.user?.id)
+            }
+        })
+
+        if(!spaceDb) {
+            return NextResponse.json({
+                message: "No space found for the user"
+            }, {
+                status: 404
+            })
+        }
+
+        return NextResponse.json({
+            spaces: spaceDb
+        })
+    } catch (error) {
+        return NextResponse.json({
+            message: "Error occured while showing users space"
         }, {
             status: 500
         })
